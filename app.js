15bf5143f1c34c0c2766c8081ff97ff8cf49e7df
Brandon Chee <brandonch9029@gmail.com>
2022-09-18 01:31:21 +0800

    Added hero banner
    Typing Animation on hero banner, began work on resume.html

---
+++ app.js
@@ -0,0 +1,13 @@
+var options = {
+    strings: [
+      'Brandon Chee', 
+      'an aspiring software engineer', 
+      'a craft beer geek', 
+      'an adventurous cook'
+    ],
+    typeSpeed: 70,
+    backSpeed: 30,
+    loop: true
+  };
+  
+  var typed = new Typed('.vartext', options);
